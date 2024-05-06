a94b20d93d64b22f68bcaa08c8a4c7243d3112f5
ezrapelealu <pelealuezra3@gmail.com>
2024-05-06 11:50:36 +0800

    Fix TaskPage and adding TextInput2,TextInputDateTime

--- src/components/molecules/PageHeader/index.tsx
+++ src/components/molecules/PageHeader/index.tsx
@@ -1,59 +1,89 @@
-import {StyleSheet, Text, View, Image} from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import React from 'react';
-import {Button} from '../../atoms';
+import { Button } from '../../atoms';
+import {useNavigation} from '@react-navigation/native';
 
-const PageHeader = ({label, backButton, onPress, type}) => {
-  if (type === 'withPhoto') {
+const PageHeader = ({ label, backButton, onPress, type, }) => {
+  const navigation = useNavigation();
+  if (type === 'withLogo') {
     return (
       <View style={styles.containerTop}>
-        <Text style={styles.title}>To Do List</Text>
-        <View style={styles.containerwithPhoto}>
-        </View>
         <Image
           style={styles.profilePic}
           source={require('../../../assets/icon/ProfileLogo.png')}
         />
-        <Image style={styles.clockLogo} source={require('../../../assets/icon/ClockLogo.png')}/>
+        <Text style={styles.title}>To Do List</Text>
+        <View style={styles.containerwithPhoto}></View>
+        <Image
+          style={styles.clockLogo}
+          source={require('../../../assets/icon/ClockLogo.png')}
+        />
       </View>
     );
   }
-  if (label === 'HomePage') {
+  if (type === 'withLogo2') {
     return (
-      <View style={styles.container}>
-        <Text style={styles.label}>{label}</Text>
+      <View style={styles.containerTop}>
+        <PageHeader
+          style={styles.backButton}
+          backButton={true}
+          onPress={() => navigation.goBack()}
+        />
+        <Text style={styles.title2}>Task</Text>
+        {/* <View style={styles.container}>
+          {backButton && (
+            <Button type="icon-only" icon="icon-back" onPress={onPress} style={[styles.backButton, style]} />
+          )}
+          <Text style={styles.label}>{label}</Text>
+        </View> */}
+        <Image
+          style={styles.clockLogo}
+          source={require('../../../assets/icon/ClockLogo.png')}
+        />
       </View>
     );
   }
+  // if (label === 'HomePage') {
+  //   return (
+  //     <View style={styles.container}>
+  //       <Text style={styles.label}>{label}</Text>
+  //     </View>
+  //   );
+  // }
   return (
     <View style={styles.container}>
       {backButton && (
-        <Button type="icon-only" icon="icon-back" onPress={onPress} />
+        <Button type="icon-only" icon="icon-back" onPress={onPress} style={styles.backButton} />
       )}
       <Text style={styles.label}>{label}</Text>
     </View>
   );
 };
 
+
 export default PageHeader;
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#FFFFFF',
-    paddingLeft: 24,
-    paddingVertical: 37,
+    marginRight: 70,
+    paddingLeft: 1,
+    paddingVertical: 5,
     flexDirection: 'row',
     alignItems: 'center',
   },
   label: {
     fontFamily: 'Poppins-Medium',
-    fontSize: 22,
+    fontSize: 20,
     color: '#020202',
-    marginLeft: 24,
-  },
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    textAlign: 'center',
+  },  
   containerwithPhoto: {
     backgroundColor: '#FFFFFF',
     paddingVertical: 37,
-    flexDirection: 'column', 
+    flexDirection: 'column',
   },
   labelWithBack: {
     fontFamily: 'Poppins-Medium',
@@ -67,6 +97,16 @@ const styles = StyleSheet.create({
     color: '#001D35',
     textAlign: 'center',
     fontWeight: 'bold',
+    // marginLeft: 30,
+    marginRight: 'auto',
+  },
+  title2: {
+    fontFamily: 'Poppins-SemiBold',
+    fontSize: 20,
+    color: '#001D35',
+    textAlign: 'center',
+    // marginLeft: 30,
+    marginRight: 'auto',
   },
   text: {
     color: '#020202',
@@ -74,13 +114,12 @@ const styles = StyleSheet.create({
   profilePic: {
     width: 60,
     height: 60,
-    marginLeft: 30,
     marginRight: 'auto',
     borderWidth: 10,
     borderRadius: 10,
     // borderColor: 'black',
   },
-  clockLogo : { 
+  clockLogo: {
     marginRight: 30,
     borderWidth: 10,
     borderRadius: 10,
@@ -89,11 +128,21 @@ const styles = StyleSheet.create({
   containerTop: {
     backgroundColor: '#FFFFFF',
     // paddingLeft: 24,
-    // paddingVertical: 37,
+    paddingVertical: 37,
     flexDirection: 'row',
     alignItems: 'center',
+    paddingHorizontal: 24,
     // flexDirection: 'row',
     // alignItems: 'center',
     // marginLeft: 26,
   },
+
+  // backButton: {
+  //   width: 60,
+  //   height: 60,
+  //   marginRight: 'auto',
+  //   borderWidth: 10,
+  //   borderRadius: 10,
+  //   marginLeft: 1,
+  // },
 });
